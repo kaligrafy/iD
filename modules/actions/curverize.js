ea58ef209dde8f9e74bfb7f8c40f4e1422cfe382
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2023-01-04 14:47:22 -0500

    initial commit

--- modules/actions/curverize.js
+++ modules/actions/curverize.js
@@ -1,6 +1,20 @@
-import { osmNode } from '../osm/node';
-import { osmWay } from '../osm/way';
-import { geoRotate, geoVecAngle, geoVecLength, geoVecAngleBetween, geoVecAdd, geoVecNormalize, geoInfiniteLineIntersection, geoVecScale, geoVecSubtract } from '../geo';
+import {
+    osmNode
+} from '../osm/node';
+import {
+    osmWay
+} from '../osm/way';
+import {
+    geoRotate,
+    geoVecAngle,
+    geoVecLength,
+    geoVecAngleBetween,
+    geoVecAdd,
+    geoVecNormalize,
+    geoInfiniteLineIntersection,
+    geoVecScale,
+    geoVecSubtract
+} from '../geo';
 
 export function actionCurverize(selectedIds, projection) {
 
@@ -12,11 +26,34 @@ export function actionCurverize(selectedIds, projection) {
             return graph.entity(selectedID);
         });
 
+        const entitiesNodes = entities.filter((entity) => entity.type === 'node');
+        const entitiesWays = entities.filter((entity) => entity.type === 'way');
+        let way = null;
+
+        if (entitiesWays.length === 0) {
+            const node1ParentWays = graph.parentWays(entitiesNodes[0]);
+            const node2ParentWays = graph.parentWays(entitiesNodes[1]);
+            const parentWaysIntersection = node1ParentWays.filter(way => {
+                return node2ParentWays.includes(way);
+            });
+            way = parentWaysIntersection[0];
+        } else {
+            way = entitiesWays[0];
+        }
+
+        const node1Idx = way.nodes.indexOf(entitiesNodes[0].id);
+        const node2Idx = way.nodes.indexOf(entitiesNodes[1].id);
+        const nodeStart = node2Idx > node1Idx ? entitiesNodes[0] : entitiesNodes[1];
+        const nodeEnd = node2Idx > node1Idx ? entitiesNodes[1] : entitiesNodes[0];
+        const nodeStartIdx = way.nodes.indexOf(nodeStart.id);
+        const nodeEndIdx = way.nodes.indexOf(nodeEnd.id);
+
+        console.log('node1Idx node2Idx', node1Idx, node2Idx);
+        console.log('nodeStartIdx nodeEndIdx', nodeStartIdx, nodeEndIdx);
         //console.log('entities', entities);
 
         // get the way and its last node to curverize:
-        let way = null;
-        let lastNode = null;
+        /*let lastNode = null;
         if (selectedIds.length === 2 && entities[0].type === 'way' && entities[1].type === 'node') {
             way = entities[0];
             lastNode = entities[1];
@@ -27,35 +64,116 @@ export function actionCurverize(selectedIds, projection) {
         } else if (selectedIds.length === 1 && entities[0].type === 'way') {
             way = entities[0];
             lastNode = graph.entity(entities[0].nodes[entities[0].nodes.length - 1]);
+            selectedNode = lastNode;
         }
-        const lastNodeIdx = way.nodes.indexOf(lastNode.id);
+        const lastNodeIdx = way.nodes.indexOf(lastNode.id);*/
 
         //console.log('lastNodeIdx', lastNodeIdx);
+        const countSegments = nodeEndIdx - nodeStartIdx;
+
+        for (let segmentI = 0; segmentI < countSegments; segmentI++) {
+
+            const segmentNodeStartIdx = nodeStartIdx + segmentI;
+            const segmentNodeEndIdx = segmentNodeStartIdx + 1;
+
+            const segmentNodePrevious = graph.entity(way.nodes[segmentNodeStartIdx - 1]);
+            const segmentNodeStart = graph.entity(way.nodes[segmentNodeStartIdx]);
+            const segmentNodeEnd = graph.entity(way.nodes[segmentNodeEndIdx]);
+            const segmentNodeNext = graph.entity(way.nodes[segmentNodeEndIdx + 1]);
+
+            const fourPoints = [segmentNodePrevious, segmentNodeStart, segmentNodeEnd, segmentNodeNext].map(function (n) {
+                return projection(n.loc);
+            });
+
+            const tangent1Line = [fourPoints[0], fourPoints[1]];
+            const tangent2Line = [fourPoints[3], fourPoints[2]];
+            const tangent1Vector = geoVecSubtract(tangent1Line[1], tangent1Line[0]);
+            const tangent2Vector = geoVecSubtract(tangent2Line[1], tangent2Line[0]);
+            const tangent1UnitVector = geoVecNormalize(tangent1Vector);
+            const tangent2UnitVector = geoVecNormalize(tangent2Vector);
+            // find intersection of the tangents:
+            let tangentsIntersection = geoInfiniteLineIntersection(tangent1Line, tangent2Line);
+            if (!tangentsIntersection) {
+                console.log('could not find line segments intersection, they may be parallel')
+                return graph;
+            }
+
+            const tangent1LengthToIntersection = geoVecLength(tangent1Line[1], tangentsIntersection);
+            const tangent2LengthToIntersection = geoVecLength(tangent2Line[1], tangentsIntersection);
+            const minLengthToIntersection = Math.min(tangent1LengthToIntersection, tangent2LengthToIntersection);
+
+            const tangent1MinifiedLine = [tangentsIntersection, geoVecSubtract(tangentsIntersection, geoVecScale(tangent1UnitVector, minLengthToIntersection))];
+            const tangent2MinifiedLine = [tangentsIntersection, geoVecSubtract(tangentsIntersection, geoVecScale(tangent2UnitVector, minLengthToIntersection))];
+
+            const tangent1PerpendicularMinifiedLine = geoRotate(tangent1MinifiedLine, -Math.PI / 2, tangent1MinifiedLine[1]);
+            const tangent2PerpendicularMinifiedLine = geoRotate(tangent2MinifiedLine, Math.PI / 2, tangent2MinifiedLine[1]);
+
+            // find intersection of the tangents:
+            let circleCenter = geoInfiniteLineIntersection(tangent1PerpendicularMinifiedLine, tangent2PerpendicularMinifiedLine);
+            if (!circleCenter) {
+                console.log('could not find a circle center to draw arc')
+                return graph;
+            }
+            /*const nodeTangentIntersection = osmNode({ loc: projection.invert(tangentsIntersection), tags: { entrance: 'shop' } });
+            graph = graph.replace(nodeTangentIntersection);
+
+            const nodeCircleCenter = osmNode({ loc: projection.invert(circleCenter) });
+            graph = graph.replace(nodeCircleCenter);*/
+
+            const radiusLineStart = [circleCenter, tangent1MinifiedLine[1]];
+            const radiusLineEnd = [circleCenter, tangent2MinifiedLine[1]];
+            const radius = geoVecLength(radiusLineStart[0], radiusLineStart[1]);
+            let angleRadiusLineStart = geoVecAngle(radiusLineStart[0], radiusLineStart[1]);
+            let angleRadiusLineEnd = geoVecAngle(radiusLineEnd[0], radiusLineEnd[1]);
+            let arcAngleRad = Math.abs(geoVecAngleBetween(angleRadiusLineStart, angleRadiusLineEnd));
+            const arcAngleDeg = arcAngleRad * 180.0 / Math.PI;
+            const arcLength = arcAngleRad * radius;
+            //const arcSegmentLength = arcAngleRad * radius / numberOfSegments;
+            console.log('segment' + (segmentI + 1), 'arcAngleRadDegLength', arcAngleRad, arcAngleDeg, arcLength);
+
+            let numberOfSegments = Math.max(2, Math.ceil(arcAngleDeg / 10.0));
+            if (arcSegmentLength > 20) {
+                numberOfSegments = Math.max(2, arcLength / 20);
+            }
+
+            const radiusNodes = [];
+
+            const directionCheckAngle = angleRadiusLineStart - geoVecAngle(tangent1MinifiedLine[0], tangent1MinifiedLine[1]);
+            const reverse = false;
+            // verify that we do go in the right direction. If angle is not -90 degrees, we need to switch direction for arc creation
+            //if (Math.round(1000.0 * directionCheckAngle) !== -1571.0) {
+            //reverse = true;
+            //}
+            console.log(reverse, 'angleBetweenTangeantAndRadiusLine', Math.round(1000.0 * directionCheckAngle), (180 / Math.PI) * directionCheckAngle);
+
+        }
 
         // get four last nodes of way (the nodes to use for the curve):
-        let lastFourNodesIds = [];
+        /*let lastFourNodesIds = [];
         let lastFourNodes = [];
         let direction = 'forward';
         if (lastNodeIdx === 0) {
             lastFourNodesIds = [way.nodes[3], way.nodes[2], way.nodes[1], way.nodes[0]];
             lastFourNodes = [graph.entity(way.nodes[3]), graph.entity(way.nodes[2]), graph.entity(way.nodes[1]), graph.entity(way.nodes[0])];
-        } else if (lastNodeIdx === way.nodes.length - 1) {
-            lastFourNodesIds = [way.nodes[way.nodes.length - 4], way.nodes[way.nodes.length - 3], way.nodes[way.nodes.length - 2], way.nodes[way.nodes.length - 1]];
-            lastFourNodes = [graph.entity(way.nodes[way.nodes.length - 4]), graph.entity(way.nodes[way.nodes.length - 3]), graph.entity(way.nodes[way.nodes.length - 2]), graph.entity(way.nodes[way.nodes.length - 1])];
+        } else if (lastNodeIdx === way.nodes.length - 1 || way.nodes[lastNodeIdx - 3]) {
+            lastFourNodesIds = [way.nodes[lastNodeIdx - 3], way.nodes[lastNodeIdx - 2], way.nodes[lastNodeIdx - 1], way.nodes[lastNodeIdx]];
+            lastFourNodes = [graph.entity(lastFourNodesIds[0]), graph.entity(lastFourNodesIds[1]), graph.entity(lastFourNodesIds[2]), graph.entity(lastFourNodesIds[3])];
             lastFourNodesIds = lastFourNodesIds.reverse();
             lastFourNodes = lastFourNodes.reverse();
             direction = 'backward';
         }
-        const lastFourPoints = lastFourNodes.map(function(n) { return projection(n.loc); });
+        const lastFourPoints = lastFourNodes.map(function(n) { return projection(n.loc); });*/
 
         //console.log('lastFourNodesIds', lastFourNodesIds);
 
 
 
+
+
         // get the angles of the first and last pairs of nodes:
-        const angle1 = geoVecAngle(lastFourPoints[0], lastFourPoints[1]);
-        const angle2 = geoVecAngle(lastFourPoints[2], lastFourPoints[3]);
-        const distanceBetweenTangents = geoVecLength(lastFourPoints[1], lastFourPoints[2]);
+        //const angle1 = geoVecAngle(lastFourPoints[0], lastFourPoints[1]);
+        //const angle2 = geoVecAngle(lastFourPoints[2], lastFourPoints[3]);
+        //const distanceBetweenTangents = geoVecLength(lastFourPoints[1], lastFourPoints[2]);
         //console.log('angle1', angle1, 'angle2', angle2, 'distanceBetweenTangents', distanceBetweenTangents);
         //const tangent1Length = geoVecLength(lastFourPoints[0], lastFourPoints[1]);
         //const tangent2Length = geoVecLength(lastFourPoints[2], lastFourPoints[3]);
@@ -72,103 +190,68 @@ export function actionCurverize(selectedIds, projection) {
         //console.log('tangent2LineScaled', tangent2Scaled);
 
 
-        const tangent1Line = [lastFourPoints[0], lastFourPoints[1]];
-        const tangent2Line = [lastFourPoints[3], lastFourPoints[2]];
-        const tangent1Vector = geoVecSubtract(tangent1Line[1], tangent1Line[0]);
-        const tangent2Vector = geoVecSubtract(tangent2Line[1], tangent2Line[0]);
-        const tangent1UnitVector = geoVecNormalize(tangent1Vector);
-        const tangent2UnitVector = geoVecNormalize(tangent2Vector);
 
-        // find intersection of the tangents:
-        let tangentsIntersection = geoInfiniteLineIntersection(tangent1Line, tangent2Line);
-        if (!tangentsIntersection) {
-            console.log('could not find line segments intersection, they may be parallel')
-            return graph;
-        }
 
-        const tangent1LengthToIntersection = geoVecLength(tangent1Line[1], tangentsIntersection);
-        const tangent2LengthToIntersection = geoVecLength(tangent2Line[1], tangentsIntersection);
-        const minLengthToIntersection = Math.min(tangent1LengthToIntersection, tangent2LengthToIntersection);
 
-        const tangent1MinifiedLine = [tangentsIntersection, geoVecSubtract(tangentsIntersection, geoVecScale(tangent1UnitVector, minLengthToIntersection))];
-        const tangent2MinifiedLine = [tangentsIntersection, geoVecSubtract(tangentsIntersection, geoVecScale(tangent2UnitVector, minLengthToIntersection))];
-
-        const tangent1PerpendicularMinifiedLine = geoRotate(tangent1MinifiedLine, -Math.PI/2, tangent1MinifiedLine[1]);
-        const tangent2PerpendicularMinifiedLine = geoRotate(tangent2MinifiedLine, Math.PI/2, tangent2MinifiedLine[1]);
-
-        // find intersection of the tangents:
-        let circleCenter = geoInfiniteLineIntersection(tangent1PerpendicularMinifiedLine, tangent2PerpendicularMinifiedLine);
-        if (!circleCenter) {
-            console.log('could not find a circle center to draw arc')
-            return graph;
-        }
-        /*const nodeTangentIntersection = osmNode({ loc: projection.invert(tangentsIntersection), tags: { entrance: 'shop' } });
-        graph = graph.replace(nodeTangentIntersection);
-
-        const nodeCircleCenter = osmNode({ loc: projection.invert(circleCenter) });
-        graph = graph.replace(nodeCircleCenter);*/
-
-        const radiusLineStart = [circleCenter, tangent1MinifiedLine[1]];
-        const radiusLineEnd = [circleCenter, tangent2MinifiedLine[1]];
-        const radius = geoVecLength(radiusLineStart[0], radiusLineStart[1]);
-        let angleRadiusLineStart = geoVecAngle(radiusLineStart[0], radiusLineStart[1]);
-        let angleRadiusLineEnd = geoVecAngle(radiusLineEnd[0], radiusLineEnd[1]);
-        //console.log('radius', radius);
-        let arcAngleRad = Math.abs(geoVecAngleBetween(angleRadiusLineStart, angleRadiusLineEnd));
-        //console.log('arcLength', arcAngleRad * radius);
-        
-        const arcAngleDeg = arcAngleRad * 180.0 / Math.PI;
-        //console.log('arcAngle', arcAngleRad, arcAngleDeg);
-
-        let numberOfSegments = Math.max(2, Math.ceil(arcAngleDeg / 10.0));
-        //console.log('arcLength', arcAngleRad * radius);
-        const arcSegmentLength = arcAngleRad * radius / numberOfSegments;
-        if (arcSegmentLength > 20) {
-            numberOfSegments = Math.max(2, arcAngleRad * radius / 20);
-        }
-
-        const radiusNodes = [];
-
-        const directionCheckAngle = angleRadiusLineStart - geoVecAngle(tangent1MinifiedLine[0], tangent1MinifiedLine[1]);
-        let reverse = false;
-        // verify that we do go in the right direction. If angle is not -90 degrees, we need to switch direction for arc creation
-        if (Math.round(1000.0 * directionCheckAngle) !== -1571.0) {
-            reverse = true;
-        }
-        //console.log(reverse, 'angleBetweenTangeantAndRadiusLine', Math.round(1000.0 * directionCheckAngle), (180/Math.PI) * directionCheckAngle);
         //if (angleRadiusLineStart - geoVecAngle(tangent1MinifiedLine[0], tangent1MinifiedLine[1]))
         //const secondRadiusSegment = geoRotate(radiusLineStart, - 1 * arcAngleRad / numberOfSegments, circleCenter);
         //const secondArcPoint = secondRadiusSegment[1];
         //const angleWithTangeant = 
 
-        for (let i = 0; i < numberOfSegments; i++) {
+        /*for (let i = 0; i < numberOfSegments; i++) {
             //console.log('angle ' + i, (180.0 / Math.PI) * i * arcAngleRad / numberOfSegments);
             const radiusSegment = geoRotate(radiusLineStart, (reverse ? i : -i) * arcAngleRad / numberOfSegments, circleCenter);
             const arcPoint = radiusSegment[1];
-            radiusNodes.push(osmNode({ loc: projection.invert(arcPoint) }));
-        }
+            const projectedPoint = projection.invert(arcPoint);
+            const distanceFromPreviousPoint = radiusNodes.length >= 1 ? geoVecLength(arcPoint, projection(radiusNodes[radiusNodes.length - 1].loc)) : undefined;
+            if (distanceFromPreviousPoint === undefined || distanceFromPreviousPoint >= 4) {
+                radiusNodes.push(osmNode({
+                    loc: projectedPoint
+                }));
+            }
+        }*/
         //console.log('radiusNodes', radiusNodes);
-        for (let i = 0; i < radiusNodes.length; i++) {
-            graph = graph.replace(radiusNodes[i]);
-        }
+        /*for (let i = 0; i < radiusNodes.length; i++) {
+            if (i === 0 && )
+                graph = graph.replace(radiusNodes[i]);
+        }*/
 
         //console.log('graph', graph);
 
-        const radiusNodesIds = radiusNodes.map(function(node) { return node.id });
+        /*const radiusNodesIds = radiusNodes.map(function (node) {
+            return node.id
+        });*/
+        
+        /*
         const wayNodes = [...(way.nodes)];
         //console.log('radiusNodesIds', radiusNodesIds);
         if (direction === 'forward') {
-            wayNodes.splice(2, 0, ...(radiusNodesIds.reverse()));
+            const firstRadiusNode = graph.entity(radiusNodesIds[0]);
+            if (radiusNodesIds[0])
+                wayNodes.splice(2, 0, ...(radiusNodesIds.reverse()));
             //console.log('way forward', way);
         } else if (direction === 'backward') {
-            wayNodes.splice(wayNodes.length - 2, 0, ...(radiusNodesIds.reverse()));
+            wayNodes.splice(lastNodeIdx - 1, 0, ...(radiusNodesIds.reverse()));
             //console.log('way backward', way);
         }
-        way = way.update({nodes: wayNodes});
+        */
+        // remove duplicate nodes:
+        /*const newWayNodes = [];
+        for (let i = 0, count = wayNodes.length - 1; i < count; i++) {
+            const nextNode = graph.entity(wayNodes[i + 1]);
+            const node = graph.entity(wayNodes[i]);
+            if (nextNode.loc[0] !== node.loc[0] && nextNode.loc[1] !== node.loc[1]) {
+                newWayNodes.push(wayNodes[i]);
+            }
+        }*/
+
+        /*way = way.update({
+            nodes: newWayNodes
+        });
         graph = graph.replace(way);
 
         //console.log('end curverize');
-
+        */
         return graph;
     };
 
--- modules/operations/curverize.js
+++ modules/operations/curverize.js
@@ -39,25 +39,66 @@ export function operationCurverize(selectedIDs, context) {
             return context.entity(selectedID);
         });
 
-        if (selectedIDs.length === 2 && entities[0].type === 'way' && entities[1].type === 'node') {
+        //const entitiesTypes = entities.map((entity) => entity.type);
+        const entitiesNodes = entities.filter((entity) => entity.type === 'node');
+        const entitiesWays = entities.filter((entity) => entity.type === 'way');
+
+        // two nodes from the same way:
+        /*if (entities.length === 3 && entitiesWays.length === 1 && entitiesNodes.length === 2) {
+            return true;
+        } else if (entities.length === 2 && entitiesNodes.length === 1 && entitiesWays.length === 1) {
+            return true;
+        } else if (entities.length === 1 && entitiesNodes.length === 1)
+        */
+
+        if ((selectedIDs.length === 2 && entitiesNodes.length === 2) || (selectedIDs.length === 3 && entitiesNodes.length === 2 && entitiesWays.length === 1)) {
+            let way = null;
+
+            if (entitiesWays.length === 0) {
+                const node1ParentWays = context.graph().parentWays(entitiesNodes[0]);
+                const node2ParentWays = context.graph().parentWays(entitiesNodes[1]);
+                const parentWaysIntersection = node1ParentWays.filter(way => {
+                    return node2ParentWays.includes(way);
+                });
+                way = parentWaysIntersection[0];
+            } else {
+                way = entitiesWays[0];
+            }
+
+            const node1Idx = way.nodes.indexOf(entitiesNodes[0].id);
+            const node2Idx = way.nodes.indexOf(entitiesNodes[1].id);
+            const nodeStart = node2Idx > node1Idx ? entitiesNodes[0] : entitiesNodes[1];
+            const nodeEnd = node2Idx > node1Idx ? entitiesNodes[1] : entitiesNodes[0];
+            const nodeStartIdx = way.nodes.indexOf(nodeStart.id);
+            const nodeEndIdx = way.nodes.indexOf(nodeEnd.id);
+
+            // there miust be at least one node before first and one node after last:
+            return nodeStartIdx >= 1 && nodeEndIdx < way.nodes.length;
+
+        }
+
+
+
+
+        /*if (selectedIDs.length === 2 && entities[0].type === 'way' && entities[1].type === 'node') {
             if (entities[0].contains(entities[1].id) && entities[0].nodes.length >= 4) {
-                if (entities[0].nodes.indexOf(entities[1].id) === 0 || entities[0].nodes.indexOf(entities[1].id) === entities[0].nodes.length - 1) {
+                //if (entities[0].nodes.indexOf(entities[1].id) === 0 || entities[0].nodes.indexOf(entities[1].id) === entities[0].nodes.length - 1) {
                     return true;
-                }
+                //}
             }
         } else if (selectedIDs.length === 1 && entities[0].type === 'node') {
             var nodeParentWays = context.graph().parentWays(entities[0]);
             if (nodeParentWays.length === 1 && nodeParentWays[0] && nodeParentWays[0].type === 'way' && nodeParentWays[0].nodes.length >= 4) {
-                if (nodeParentWays[0].nodes.indexOf(entities[0].id) === 0 || nodeParentWays[0].nodes.indexOf(entities[0].id) === nodeParentWays[0].nodes.length - 1) {
+                //if (nodeParentWays[0].nodes.indexOf(entities[0].id) === 0 || nodeParentWays[0].nodes.indexOf(entities[0].id) === nodeParentWays[0].nodes.length - 1) {
                     return true;
-                }
+                //}
             }
         } else if (selectedIDs.length === 1 && entities[0].type === 'way') {
             var way = entities[0];
             if (way.nodes && way.nodes.length >= 4) {
                 return true;
             }
-        }
+        }*/
 
 
 
